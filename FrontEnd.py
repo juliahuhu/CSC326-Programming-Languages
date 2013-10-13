cd584f0c5837b8275bdf9b573b41a5d65fc403eb
anmolkaur <anmol.kaur18@gmail.com>
2013-10-13 10:05:28 -0400

    Updated FrontEnd

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,37 +1,46 @@
-from bottle import route, run, request
+from bottle import route, run, request, FormsDict
 import collections
 
+
+#import Logo from another file
+with open ("Logo.txt", "r") as LogoFile:
+	LogoString = LogoFile.read().replace('\n', "<br>")
+	LogoString = "<html><pre>"+LogoString+ "</html>"
+
+
+#homepage - just show logo
 @route('/')
-def hello():
-	return "Hello World"
+def Logo():
+	return LogoString
 
 
+#search page - includes an html form with one text box for search input
 @route('/search')
 def search():
-	return '''
+	return LogoString + "<br><br>" +'''
 		<form action ="/search" method="post">
 			Search: <input name="userinput" type="text"/>
 			<input value = "Search" type="submit" />
 		</form>
 	'''
 
+#search result page
 @route('/search', method='POST')
 def do_search():
+	#read user input from form
 	userinput = request.forms.get("userinput")
-
+	
+	#split user search into words and count the occurance of each word using collections.Counter
 	words = userinput.split(" ")
-	print words
 	wordcounter = collections.Counter(words)
 	
-	
-	printWordCounter = """<table border = "0"><tr><th>Word &nbsp</th><th>Count</th></tr>"""
+	#Create a new string printWordCounter which holds the text for an HTML table include all the words and the number of times they occur in the search
+	printWordCounter = """<table border = "0"><tr><th align = "left">Word</th><th>Count</th></tr>"""
 	for key, value in zip(wordcounter.keys(), wordcounter.values()):
-		print (key +"\t"+ str(value))
-		printWordCounter += ("<tr><td>" + key + "</td><td>" + str(value) + "</td></tr>")
+		printWordCounter += ("<tr><td>" + key + """</td><td align="center">""" + str(value) + "</td></tr>")
 
-	print printWordCounter
 
-	return "Search "+  "'%s' <br><br> %s"  %(userinput, printWordCounter) 
+	return LogoString + "<br><br>" + "Search "+  "'%s' <br><br> %s"  %(userinput, printWordCounter) 
 
 
 run(host="localhost", port="8080", debug=True)
