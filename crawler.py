9cbfd344a13b7c52321e6be00c1cf0697c9ab170
Vincent Lee <leevinc7@ug177.eecg>
2013-10-21 18:21:23 -0400

    Added db stuff to crawler. Added DB Integration. Still need more work

--- crawler.py
+++ crawler.py
@@ -26,6 +26,7 @@
 
 import urllib2
 import urlparse
+import sqlite3 as lite
 from BeautifulSoup import *
 from collections import defaultdict
 import re
@@ -56,7 +57,20 @@ class crawler(object):
         self._url_queue = [ ]
         self._doc_id_cache = { }
         self._word_id_cache = { }
-
+       
+        #initialize the DB 
+        self.db_conn = db_conn
+        self.cur = self.db_conn.cursor()
+        self.cur.executescript("""
+            DROP TABLE IF EXISTS Lexicon;
+            DROP TABLE IF EXISTS DocIndex;
+            CREATE TABLE IF NOT EXISTS 
+            Lexicon(id INTEGER PRIMARY KEY, 
+                    word TEXT);
+            CREATE TABLE IF NOT EXISTS 
+            DocIndex(id INTEGER PRIMARY KEY, 
+                    url TEXT);""")
+        
         # functions to call when entering and exiting specific tags
         self._enter = defaultdict(lambda *a, **ka: self._visit_ignore)
         self._exit = defaultdict(lambda *a, **ka: self._visit_ignore)
@@ -82,7 +96,7 @@ class crawler(object):
         self._enter['h5'] = self._increase_font_factor(3)
         self._enter['title'] = visit_title
 
-        # decrease the font size when we exit these tags
+        # decrease the font size when we exself.it these tags
         self._exit['b'] = self._increase_font_factor(-2)
         self._exit['strong'] = self._increase_font_factor(-2)
         self._exit['i'] = self._increase_font_factor(-1)
@@ -127,13 +141,22 @@ class crawler(object):
                     self._url_queue.append((self._fix_url(line.strip(), ""), 0))
         except IOError:
             pass
-    
+
+    def __del__(self):    
+        if self.db_conn:
+           self.db_conn.close()
+
     # TODO remove me in real version
     def _mock_insert_document(self, url):
         """A function that pretends to insert a url into a document db table
         and then returns that newly inserted document's id."""
+
         ret_id = self._mock_next_doc_id
         self._mock_next_doc_id += 1
+        if self.db_conn:
+            self.cur.execute("""INSERT INTO Lexicon VALUES( null, '%s');""" %  url)
+            self.db_conn.commit()
+
         LINK_LIST[ret_id] = url
         return ret_id
     
@@ -141,6 +164,10 @@ class crawler(object):
     def _insert_word(self, word):
         """A function that pretends to inster a word into the lexicon db table
         and then returns that newly inserted word's id."""
+        if self.db_conn:
+            self.cur.execute("""INSERT INTO DocIndex VALUES( null, '%s');""" %  word)
+            self.db_conn.commit()
+
         ret_id = self._mock_next_word_id
         self._mock_next_word_id += 1
         WORD_LIST[ret_id] = word
@@ -366,7 +393,15 @@ class crawler(object):
         return RESOLVED_INVERTED_INDEX
 
 if __name__ == "__main__":
-    bot = crawler(None, "urls.txt")
+    db_conn = lite.connect("table.db")
+    bot = crawler(db_conn, "urls.txt")
     bot.crawl(depth=1)
-    print bot.get_inverted_index()
-    print bot.get_resolved_inverted_index()
+    #print bot.get_inverted_index()
+    #print bot.get_resolved_inverted_index()
+    cur = db_conn.cursor()
+    cur.execute('SELECT * FROM Lexicon')
+    data = cur.fetchall()
+    print  str(data)
+    cur.execute('SELECT * FROM DocIndex')
+    data = cur.fetchall()
+    print  str(data)
