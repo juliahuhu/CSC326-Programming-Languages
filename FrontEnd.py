55e825ad3fdfc62e0302fe1aa3e3347ea6dd5148
anmolkaur <anmol.kaur18@gmail.com>
2013-10-26 16:41:43 -0400

    Front End Lab 2 edits

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,5 +1,5 @@
-from bottle import route, run, request, FormsDict
-import collections
+from bottle import route, run, request, FormsDict, error
+import collections, sqlite3
 
 
 #import Logo from another file
@@ -7,6 +7,10 @@ with open ("Logo.txt", "r") as LogoFile:
 	LogoString = LogoFile.read().replace('\n', "<br>")
 	LogoString = "<html><pre>"+LogoString+ "</html>"
 
+#error page
+@error(404)
+def error404(error):
+	return '''This page or file does not exist. <br><br> Please visit the <a href="http://localhost:8080/search"> Search page</a> for a new search.'''
 
 #homepage - just show logo
 @route('/')
@@ -40,7 +44,33 @@ def do_search():
 		printWordCounter += ("<tr><td>" + key + """</td><td align="center">""" + str(value) + "</td></tr>")
 
 
-	return LogoString + "<br><br>" + "Search "+  "'%s' <br><br> %s"  %(userinput, printWordCounter) 
+	#test table
+	conn = sqlite3.connect('table.db')
+	c=conn.cursor()
+	#c.execute('''CREATE TABLE test
+	#			(date text, name text)''')
+	#c.execute('INSERT INTO test VALUES ("2013-10-05", "Anmol")')
+	#c.execute('INSERT INTO test VALUES ("2013-10-07", "Vincent")')
+	conn.commit()
+	conn.close()
+	
+	#get results from test table
+	conn = sqlite3.connect('table.db')
+	c=conn.cursor()
+	addedResult = ""
+	for row in c.execute('SELECT * FROM test ORDER BY date'):	
+		result = c.fetchone()
+		print (str(result))
+		addedResult += ("<br><br>" + str(result))
+		print (addedResult)
+
+	#Display table results on page
+
+
+
+
+	return LogoString + "<br><br>" + "Search "+  "'%s' <br><br> %s %s"  %(userinput, printWordCounter, addedResult) 
+
 
 
 run(host="localhost", port="8080", debug=True)
